56e041abb6f0cc6eb500ab837e2d1bb0a0242ad5
reisalazar <rrsb45@gmail.com>
2022-09-10 02:56:29 -0300

    FakedataSource implementation

--- starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,24 +4,45 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(var reminders: MutableList<ReminderDTO> = mutableListOf()) :
+    ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    //    Create a fake data source to act as a double to the real data source
+    private var shouldReturnError = false
+
+    fun setShouldReturnError(value: Boolean) {
+        this.shouldReturnError = value
+    }
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        if (shouldReturnError) {
+            return Result.Error("Reminders not found", 404)
+        } else {
+            return Result.Success(ArrayList(reminders))
+        }
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        //("save the reminder")
+        reminders.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        //("return the reminder with the id")
+        return try {
+            val reminder = reminders.find { it.id == id }
+            if (shouldReturnError || reminder == null) {
+                throw Exception("$id not found.")
+            } else {
+                Result.Success(reminder)
+            }
+        } catch (e: Exception) {
+            Result.Error(e.message)
+        }
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminders.clear()
     }
 
 
